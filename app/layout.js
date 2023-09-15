64b6588138029f9590bc40f53ef5e20abe233025
David Petre <davidpetre@uchicago.edu>
2023-09-14 19:27:27 -0500

    Removed tacky footer

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="font-serif bg-[#fffeea] flex justify-center">
         {children}
-        <Footer />
       </body>
     </html>
   </>
